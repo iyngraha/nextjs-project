7ba1a149c166625230b447887a0a2e03dd01f3e5
iyngraha <blueskycloud94@gmail.com>
2023-06-30 23:04:08 +0700

    under construction

---
+++ pages/property/[id].js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Box, Text, Spacer, Avatar } from '@chakra-ui/react';
+import { FaBed, FaBath } from 'react-icons/fa';
+import { BsGridFill } from 'react-icons/bs';
+import { GoVerified } from 'react-icons/go';
+import millify from 'millify';
+import { ImageScrollBar } from '../../components/ImageScrollBar';
+
+import { baseUrl, fetchApi } from '../../utils/fetchApi';
+
+const PropertyDetails = ({ propertyDetails: { price, rentFrequency, rooms, title, baths, area, agency, isVerified, description, type, purpose, finishingStatus, amenities, photos } }) => (
+    <Box maxWidth='1000px' margin='auto' p='4'>
+        {photos && <ImageScrollBar data={photos} />}
+    </Box>
+)
+
+export default PropertyDetails;
+
+export async function getServerSideProps({ params: { id } }) {
+    const data = await fetchApi(`${baseUrl}/properties/detail?externalID=${id}`)
+
+    return {
+        props: {
+            propertyDetails: data
+        }
+    }
+}
+
